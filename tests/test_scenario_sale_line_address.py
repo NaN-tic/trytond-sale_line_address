7ec12346105ea0615e47d1b860040ca03f26894c
Ismael Marcos Martínez <ismael.marcos@nan-tic.com>
2024-08-08 11:16:37 +0200

    Convert ‘test’ files to .py

---
+++ tests/test_scenario_sale_line_address.py
@@ -0,0 +1,179 @@
+import unittest
+from decimal import Decimal
+
+from proteus import Model
+from trytond.modules.account.tests.tools import (create_chart,
+                                                 create_fiscalyear,
+                                                 get_accounts)
+from trytond.modules.account_invoice.tests.tools import (
+    create_payment_term, set_fiscalyear_invoice_sequences)
+from trytond.modules.company.tests.tools import create_company, get_company
+from trytond.tests.test_tryton import drop_db
+from trytond.tests.tools import activate_modules
+
+
+class Test(unittest.TestCase):
+
+    def setUp(self):
+        drop_db()
+        super().setUp()
+
+    def tearDown(self):
+        drop_db()
+        super().tearDown()
+
+    def test(self):
+
+        # Activate sale_line_address
+        config = activate_modules('sale_line_address')
+
+        # Create company
+        _ = create_company()
+        company = get_company()
+
+        # Create sale user
+        User = Model.get('res.user')
+        Group = Model.get('res.group')
+        sale_user = User()
+        sale_user.name = 'Sale'
+        sale_user.login = 'sale'
+        sale_group, = Group.find([('name', '=', 'Sales')])
+        sale_user.groups.append(sale_group)
+        sale_user.save()
+
+        # Create stock user
+        stock_user = User()
+        stock_user.name = 'Stock'
+        stock_user.login = 'stock'
+        stock_group, = Group.find([('name', '=', 'Stock')])
+        stock_user.groups.append(stock_group)
+        stock_user.save()
+
+        # Create account user
+        account_user = User()
+        account_user.name = 'Account'
+        account_user.login = 'account'
+        account_group, = Group.find([('name', '=', 'Account')])
+        account_user.groups.append(account_group)
+        account_user.save()
+
+        # Create fiscal year
+        fiscalyear = set_fiscalyear_invoice_sequences(
+            create_fiscalyear(company))
+        fiscalyear.click('create_period')
+
+        # Create chart of accounts
+        _ = create_chart(company)
+        accounts = get_accounts(company)
+        revenue = accounts['revenue']
+        expense = accounts['expense']
+
+        # Create parties
+        Party = Model.get('party.party')
+        PartyAddress = Model.get('party.address')
+        supplier = Party(name='Supplier')
+        supplier.save()
+        customer = Party(name='Customer')
+        customer.save()
+        address1 = PartyAddress(name='a1', party=customer, delivery=True)
+        address1.save()
+        address2 = PartyAddress(name='a2', party=customer, delivery=True)
+        address2.save()
+        self.assertEqual(len(customer.addresses), 3)
+
+        # Create account category
+        ProductCategory = Model.get('product.category')
+        account_category = ProductCategory(name="Account Category")
+        account_category.accounting = True
+        account_category.account_expense = expense
+        account_category.account_revenue = revenue
+        account_category.save()
+
+        # Create product
+        ProductUom = Model.get('product.uom')
+        unit, = ProductUom.find([('name', '=', 'Unit')])
+        ProductTemplate = Model.get('product.template')
+        template = ProductTemplate()
+        template.name = 'product'
+        template.default_uom = unit
+        template.type = 'goods'
+        template.salable = True
+        template.list_price = Decimal('10')
+        template.cost_price_method = 'fixed'
+        template.account_category = account_category
+        template.save()
+        product, = template.products
+        product.save()
+
+        # Create payment term
+        payment_term = create_payment_term()
+        payment_term.save()
+
+        # Create an Inventory
+        config.user = stock_user.id
+        Inventory = Model.get('stock.inventory')
+        InventoryLine = Model.get('stock.inventory.line')
+        Location = Model.get('stock.location')
+        storage, = Location.find([
+            ('code', '=', 'STO'),
+        ])
+        inventory = Inventory()
+        inventory.location = storage
+        inventory.save()
+        inventory_line = InventoryLine(product=product, inventory=inventory)
+        inventory_line.quantity = 100.0
+        inventory_line.expected_quantity = 0.0
+        inventory.save()
+        inventory_line.save()
+        Inventory.confirm([inventory.id], config.context)
+        self.assertEqual(inventory.state, 'done')
+
+        # Sale 5 products with an invoice method 'on shipment'
+        config.user = sale_user.id
+        Sale = Model.get('sale.sale')
+        SaleLine = Model.get('sale.line')
+        sale = Sale()
+        sale.party = customer
+        sale.shipment_address = address1
+        sale.payment_term = payment_term
+        sale.invoice_method = 'shipment'
+        sale_line = SaleLine()
+        sale.lines.append(sale_line)
+        sale_line.product = product
+        sale_line.quantity = 2.0
+        sale_line = SaleLine()
+        sale.lines.append(sale_line)
+        sale_line.type = 'comment'
+        sale_line.description = 'Comment'
+        sale_line = SaleLine()
+        sale.lines.append(sale_line)
+        sale_line.product = product
+        sale_line.quantity = 3.0
+        sale_line.delivery_address = address2
+        sale.save()
+        sale.click('quote')
+        sale.click('confirm')
+        self.assertEqual(sale.state, 'processing')
+
+        sale.reload()
+        self.assertEqual(len(sale.shipments), 2)
+        self.assertEqual(len(sale.shipment_returns), 0)
+        self.assertEqual(len(sale.invoices), 0)
+
+        # Done shipments
+        shipment, shipment2 = sale.shipments
+        config.user = stock_user.id
+        ShipmentOut = Model.get('stock.shipment.out')
+        ShipmentOut.assign_try([shipment.id], config.context)
+        ShipmentOut.pick([shipment.id], config.context)
+        ShipmentOut.pack([shipment.id], config.context)
+        ShipmentOut.done([shipment.id], config.context)
+        ShipmentOut.assign_try([shipment2.id], config.context)
+        ShipmentOut.pick([shipment2.id], config.context)
+        ShipmentOut.pack([shipment2.id], config.context)
+        ShipmentOut.done([shipment2.id], config.context)
+
+        # Check invoice
+        config.user = sale_user.id
+        sale.reload()
+        self.assertEqual(len(sale.invoices), 2)
